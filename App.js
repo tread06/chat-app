4ff26041a6b9efc8e9b6faee25f7b35772861300
Tanner Read <tread06@gmail.com>
2021-11-18 18:10:57 -0500

    Added Firebase authentication and message database
    Added firebase info as environment variables. Added automatic anonymous authentication and functionality for string messages in the firestore database.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import ChatApp from './components/chat-app';
 
 export default function App() {
+  LogBox.ignoreAllLogs();
   return <ChatApp />;
 }
--- components/chat-screen.jsx
+++ components/chat-screen.jsx
@@ -1,46 +1,96 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
-
+import { db, auth} from '../firebase-config';
+import { signInAnonymously, onAuthStateChanged} from 'firebase/auth';
+import { collection, onSnapshot, doc, query, setDoc } from 'firebase/firestore';
 
 export default function ChatScreen(props){
 
   const [name, setName] = useState('');
   const [color, setColor] = useState('');
-  const [messages, setMessages] = useState([]);  
+  const [messages, setMessages] = useState([]);    
+  const [user, setUser] = useState(""); 
 
   //on mount, setup navigation options and store name in state
   useEffect(()=>{    
     const nameProp = props.route.params.name;
     const colorProp = props.route.params.color;
     props.navigation.setOptions({ title: nameProp });
+
     setName(nameProp);
     setColor(colorProp);
 
-    //add messages
-    setMessages([
-      {
-        _id: 1,
-        text: nameProp + ' has entered the chat!',
-        createdAt: new Date(),
-        system: true,
-      },
-      {
-        _id: 2,
-        text: 'Welcome, ' + nameProp,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      }
-    ]);
+    let messagesUnsubscribe = () => {};
+
+    if(user === ""){
+      //if no user is signed in, sign in anonymously
+      const signIn = async () =>{        
+        try{
+          await signInAnonymously(auth);          
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }      
+      signIn();
+
+      const q = query(collection(db, "messages"));
+      messagesUnsubscribe = onSnapshot(q, (OnMessagesUpdated));
+    }
+
+    return () => {      
+      messagesUnsubscribe();
+    }
   }, [])   
 
-  const onSend = (newMessages = []) => {    
-    setMessages(GiftedChat.append(messages, newMessages));
+  onAuthStateChanged(auth, (newUser) => {
+    if (newUser) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User          
+      setUser(newUser.uid);   
+    } else {
+      // User is signed out  
+      setUser("");
+    }
+  });
+
+  function onSend(newMessages = []){ 
+
+    //add message to database
+    newMessages.forEach((message) => {            
+      setDoc(doc(db, "messages", message._id), {
+        _id: message._id,
+        text: message.text,
+        createdAt: message.createdAt,
+        user: {
+          _id: user,
+          name: name
+        }
+      });
+    });
+  }
+
+  function OnMessagesUpdated(snapshot){
+
+    //update messages state
+    const newMessages = [];    
+    snapshot.forEach((doc) => {      
+      let data = doc.data();         
+      newMessages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user
+      });
+    });
+
+    //sort messages --- most recent on the bottom
+    newMessages.sort(function (a, b) {
+      return b.createdAt - a.createdAt;
+    });
+    setMessages(newMessages);
   }
 
   const renderBubble = (props) =>{
@@ -48,8 +98,13 @@ export default function ChatScreen(props){
       <Bubble
         {...props}
         wrapperStyle={{
-          right: {
-            backgroundColor: color
+          right: {            
+            //backgroundColor: 'color' 
+            marginTop: 15          
+          },
+          left: {            
+            //backgroundColor: 'color' 
+            marginTop: 15          
           }
         }}
       />
@@ -57,13 +112,15 @@ export default function ChatScreen(props){
   }
 
   return (    
-    <View style={[{backgroundColor: 'white'}, styles.container]}>
+    <View style={[{backgroundColor: color}, styles.container]}>
       <GiftedChat
+        // loadEarlier={true}
         renderBubble={renderBubble.bind(this)}
         messages={messages}
         onSend={messages => onSend(messages)}
         user={{
-          _id: 1,
+          _id: user,
+          name: name
         }}
       />
 
