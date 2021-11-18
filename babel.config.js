743f491b720fce68fa2397b9361869c8f45823ad
Tanner Read <tread06@gmail.com>
2021-11-18 18:18:05 -0500

    Cleaned up comments in babel.config.js
    Cleaned up comments in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,24 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    //presets: ['babel-preset-expo', 'module:react-native-dotenv'],
-    //presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    //presets: ['module:metro-react-native-babel-preset'],
-    // plugins: [
-    //   [
-    //     'module:react-native-dotenv',
-    //     {
-    //       moduleName: '@env',
-    //       path: '.env',
-    //       blacklist: null,
-    //       whitelist: null,
-    //       safe: false,
-    //       allowUndefined: true,
-    //     },
-    //   ],
-    // ],
-    plugins: [
-      "module:react-native-dotenv",
-    ]
+    plugins: ['module:react-native-dotenv'],
   };
 };
